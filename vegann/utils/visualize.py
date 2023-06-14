75720b32516e77f6db4da62644696e17a4a3c7ed
Simon Madec <simon.madec@cirad.fr>
2023-06-14 13:30:24 +0200

    Update visualize.py

--- vegann/utils/visualize.py
+++ vegann/utils/visualize.py
@@ -1,35 +1,25 @@
-import datetime
-import glob
-import os
-import random
-import shutil
-from pathlib import Path
 
 import numpy as np
-import pandas as pd
-from geojson import Feature, FeatureCollection, Point, dump
-from PIL import Image
-from skimage.io import imread
 
+
+# take Image as input (im) with 0 1 boolean mask (X_true) 
+# and alpha_ground, alpha_veg that indicates the degree of transparency
+# for ground mask (background) and vegetation mask (vegetation)
 
 def colorTransform_VegGround(
-    im: np.ndarray, X_true: np.ndarray, alpha_vert: float, alpha_g: float
+    im: np.ndarray, X_true: np.ndarray, alpha_ground: float, alpha_veg: float
 ):
-    """ """
 
-    alpha = alpha_vert
     color = [97, 65, 38]
-    # color = [x / 255 for x in color]
     image = np.copy(im)
     for c in range(3):
         image[:, :, c] = np.where(
-            X_true == 0, image[:, :, c] * (1 - alpha) + alpha * color[c], image[:, :, c]
+            X_true == 0, image[:, :, c] * (1 - alpha_veg) + alpha_veg * color[c], image[:, :, c]
         )
-    alpha = alpha_g
+  
     color = [34, 139, 34]
-    #    color = [x / 255 for x in color]
     for c in range(3):
         image[:, :, c] = np.where(
-            X_true == 1, image[:, :, c] * (1 - alpha) + alpha * color[c], image[:, :, c]
+            X_true == 1, image[:, :, c] * (1 - alpha_ground) + alpha_ground * color[c], image[:, :, c]
         )
     return image
